c2d994655ca1b46fec86b7c7908b2d26e9803a65
Daniel <danielchua93@gmail.com>
2021-12-16 18:55:38 +0800

    first commit

---
+++ src/main/java/sg/edu/iss/team8/leaveApp/Team8LeaveApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.team8.leaveApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team8LeaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team8LeaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/iss/team8/leaveApp/Team8LeaveApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.team8.leaveApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team8LeaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
