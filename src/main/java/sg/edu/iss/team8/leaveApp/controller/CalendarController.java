49de88c55fe335b1320a5bffb747fc386a5f5bc3
Zhe Rui Tan <32306977+malcolmtzr@users.noreply.github.com>
2021-12-21 15:13:57 +0800

    Working on REST Controller

---
+++ src/main/java/sg/edu/iss/team8/leaveApp/controller/CalendarController.java
@@ -0,0 +1,50 @@
+package sg.edu.iss.team8.leaveApp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sg.edu.iss.team8.leaveApp.model.Leave;
+import sg.edu.iss.team8.leaveApp.repo.LeaveRepo;
+import sg.edu.iss.team8.leaveApp.service.LeaveService;
+import sg.edu.iss.team8.leaveApp.service.LeaveServiceImpl;
+import sg.edu.iss.team8.leaveApp.validator.LeaveValidator;
+
+@CrossOrigin(origins = "SOMESOME")
+@RestController
+@RequestMapping("/calendarAPI")
+public class CalendarController {
+
+	@Autowired
+	private LeaveRepo lrepo;
+	
+	@Autowired
+	private LeaveService lservice;
+	
+	@Autowired
+	public void setLeaveService(LeaveServiceImpl lserviceImpl) {
+		this.lservice = lserviceImpl;
+	}
+	
+	@Autowired
+	private LeaveValidator lValidator;
+	
+	@InitBinder("leave")
+	private void initLeaveBinder(WebDataBinder binder) {
+		binder.addValidators(lValidator);
+	}
+	
+	@GetMapping("/leaves")
+	public List<Leave> getLeaves() {
+		return lrepo.findAll();
+	}
+	
+	
+	
+}
