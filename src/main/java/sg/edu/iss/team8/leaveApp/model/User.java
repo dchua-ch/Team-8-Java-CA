25feb0f02d6a29244ecf5ac6b0901523d7b909a5
glenna <glennatjt@gmail.com>
2021-12-21 23:18:48 +0800

    login with spring security

--- src/main/java/sg/edu/iss/team8/leaveApp/model/User.java
+++ src/main/java/sg/edu/iss/team8/leaveApp/model/User.java
@@ -2,6 +2,8 @@ package sg.edu.iss.team8.leaveApp.model;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorColumn;
@@ -13,7 +15,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,15 +27,17 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @NoArgsConstructor
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="user_type", discriminatorType = DiscriminatorType.STRING)
-public abstract class User {
+@Table(name="user")
+public class User {
 	@Id
 	@GeneratedValue (strategy = GenerationType.AUTO)
 	public Integer userId;
 	private String name;
 	private String username;
 	private String password;
+	private String role;
+	private boolean enabled;
+	
 
 	public User(String name) {
 		this.name = name;
@@ -41,5 +49,5 @@ public abstract class User {
 		this.username = username;
 		this.password = password;
 	}
-	
+
 }
--- src/main/java/sg/edu/iss/team8/leaveApp/repo/UserRepo.java
+++ src/main/java/sg/edu/iss/team8/leaveApp/repo/UserRepo.java
@@ -1,6 +1,7 @@
 package sg.edu.iss.team8.leaveApp.repo;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -9,21 +10,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import sg.edu.iss.team8.leaveApp.model.Employee;
-import sg.edu.iss.team8.leaveApp.model.Manager;
 import sg.edu.iss.team8.leaveApp.model.User;
+
 
 public interface UserRepo extends JpaRepository<User, Integer>{
 	
 	@Transactional
-	@Modifying
-	@Query(value = "update User U set U.user_type = :usertype where u.name = :name",nativeQuery=true)
-	public void updateUserType(@Param("usertype") String usertype, @Param("name") String name);
-	
-	@Query("select U from User U where TYPE(U) = 'employee'")
-	public List<Employee> getAllEmployees();
-	
-	@Query("select U from User U where TYPE(U) = 'manager'")
-	public List<Manager> getAllManagers();
+
+	@Query("SELECT u FROM User u WHERE u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
 	
 }
