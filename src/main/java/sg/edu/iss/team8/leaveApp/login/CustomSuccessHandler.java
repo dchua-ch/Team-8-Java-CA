69456016235a1e11504c59f4104c52aad4dadb23
glenna <glennatjt@gmail.com>
2021-12-22 09:49:49 +0800

    add Custom Success Handler

---
+++ src/main/java/sg/edu/iss/team8/leaveApp/login/CustomSuccessHandler.java
@@ -0,0 +1,81 @@
+package sg.edu.iss.team8.leaveApp.login;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+/*@Component
+public class CustomSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+
+		String redirectUrl = null;
+
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		for (GrantedAuthority grantedAuthority : authorities) {
+			System.out.println("role " + grantedAuthority.getAuthority());
+			if (grantedAuthority.getAuthority().equals("MANAGER")) {
+				redirectUrl = "/dashboard";
+				break;
+			} else if (grantedAuthority.getAuthority().equals("ADMIN")) {
+				redirectUrl = "/adminDashboard";
+				break;
+			}
+		}
+		System.out.println("redirectUrl " + redirectUrl);
+		if (redirectUrl == null) {
+			throw new IllegalStateException();
+		}
+		new DefaultRedirectStrategy().sendRedirect(request, response, redirectUrl);
+	}
+}*/
+
+@Component
+public class CustomSuccessHandler implements AuthenticationSuccessHandler {
+
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest arg0, HttpServletResponse arg1,
+			Authentication authentication) throws IOException, ServletException {
+
+		boolean hasEmployeeRole = false;
+		boolean hasAdminRole = false;
+		boolean hasManagerRole = false;
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		for (GrantedAuthority grantedAuthority : authorities) {
+			if (grantedAuthority.getAuthority().equals("ROLE_EMPLOYEE")) {
+				hasEmployeeRole = true;
+				break;
+			} else if (grantedAuthority.getAuthority().equals("ROLE_ADMIN")) {
+				hasAdminRole = true;
+				break;
+			}
+			else if (grantedAuthority.getAuthority().equals("ROLE_MANAGER")) {
+				hasManagerRole = true;
+				break;
+			}
+		}
+
+		if (hasEmployeeRole || hasManagerRole) {
+			redirectStrategy.sendRedirect(arg0, arg1, "/dashboard");
+		} else if (hasAdminRole) {
+			redirectStrategy.sendRedirect(arg0, arg1, "/adminDashboard");
+		} else {
+			throw new IllegalStateException();
+		}
+	}
+
+}
